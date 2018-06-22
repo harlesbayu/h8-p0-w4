4851d824162cc4b402c6c399c8baea63414d1250
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2018-06-22 16:09:35 +0700

    Last Update

--- Exercises11.js
+++ Exercises11.js
@@ -1,6 +1,10 @@
 /*
 Problem
-Diberikan sebuah function changeMe(arr) yang menerima satu parameter berupa array multidimensi dimana array tersebut berisi value (pasti berurut dari kiri ke kanan) First Name, Last Name, Gender dan Birth Year . Fungsi ini akan menampilkan object literal yang memiliki property firstName, lastName, gender dan age. Nilai age didapatkan dari tahun sekarang dikurang tahun lahir. Jika tahun lahir tidak diisi atau tahun lahir lebih besar dibandingkan tahun sekarang maka age akan berisi 'Invalid Birth Year'
+Diberikan sebuah function changeMe(arr) yang menerima satu parameter berupa array multidimensi dimana array tersebut 
+berisi value (pasti berurut dari kiri ke kanan) First Name, Last Name, Gender dan Birth Year . 
+Fungsi ini akan menampilkan object literal yang memiliki property firstName, lastName, gender dan age. 
+Nilai age didapatkan dari tahun sekarang dikurang tahun lahir. Jika tahun lahir tidak diisi atau tahun lahir lebih 
+besar dibandingkan tahun sekarang maka age akan berisi 'Invalid Birth Year'
 
 Contoh jika arr inputan adalah [['Platinum', 'Fox', 'female', 1995], ['John', 'Doe', 'male', 2000]] maka output:
 
